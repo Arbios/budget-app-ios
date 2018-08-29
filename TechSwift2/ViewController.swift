3f9776cd3a486af26827154e82bab328600d61c1
Arbi Bashaev <arbi.personal@gmail.com>
2018-08-30 01:03:28 +0300

    Initial Commit

---
+++ TechSwift2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechSwift2
+//
+//  Created by ARBI BASHAEV on 30/08/2018.
+//  Copyright © 2018 ARBI BASHAEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
